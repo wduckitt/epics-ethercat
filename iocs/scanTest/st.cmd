e36221a09606ed3a7786f534f8d4cfb1cf2e056a
William Duckitt <wdduckitt@sun.ac.za>
2024-03-27 13:17:06 +0200

    updated the st.cmd

--- iocs/scanTest/st.cmd
+++ iocs/scanTest/st.cmd
@@ -1,17 +1,18 @@
-#!bin/linux-x86/scantest
+#!bin/linux-x86_64/scantest
 dbLoadDatabase("dbd/scantest.dbd")
 scantest_registerRecordDeviceDriver(pdbbase)
 
-ADC_Ethercat_Sampler("RF0", 1, "Ch1Sample.Ch1Value", "Ch1CycleCount.Ch1CycleCount")
+#ADC_Ethercat_Sampler("RF0", 1, "Ch1Sample.Ch1Value", "Ch1CycleCount.Ch1CycleCount")
 
-ecAsynInit("/tmp/scan1", 1000000)
+ecAsynInit("/tmp/socket0", 1000000)
 
-dbLoadRecords("../../db/MASTER.template", "DEVICE=JRECTEST:0,PORT=MASTER0,SCAN=I/O Intr")
-#dbLoadRecords("../../db/EK1101.template", "DEVICE=JRECTEST:1,PORT=COUPLER0,SCAN=I/O Intr")
-#dbLoadRecords("../../db/EL1004.template", "DEVICE=JRECTEST:2,PORT=VACUUM0,SCAN=I/O Intr")
-#dbLoadRecords("../../db/EL3702.template", "DEVICE=JRECTEST:3,PORT=RF0,SCAN=I/O Intr")
-#dbLoadRecords("../../db/EL2004.template", "DEVICE=JRECTEST:4,PORT=OUT0,SCAN=I/O Intr")
-dbLoadRecords("../../db/gadc.template", "DEVICE=JRECTEST:3,PORT=RF0,SCAN=.1 second,CHANNEL=1,SAMPLES=100840,INTSCAN=.1 second")
-dbLoadRecords("../../db/xfc.template", "DEVICE=JRECTEST:3,PORT=RF0,SCAN=.1 second,CHANNEL=1")
+dbLoadRecords("../../db/MASTER.template", "DEVICE=ECTest:0,PORT=MASTER0,SCAN=I/O Intr")
+dbLoadRecords("../../ethercatApp/Db/EK1818.template", "DEVICE=ECTest:1,PORT=ERIO.0,SCAN=I/O Intr")
+dbLoadRecords("../../ethercatApp/Db/EL6090.template", "DEVICE=ECTest:2,PORT=ERIO.1,SCAN=I/O Intr")
+#dbLoadRecords("../../db/EL1004.template", "DEVICE=ECTest:2,PORT=VACUUM0,SCAN=I/O Intr")
+#dbLoadRecords("../../db/EL3702.template", "DEVICE=ECTest:3,PORT=RF0,SCAN=I/O Intr")
+#dbLoadRecords("../../db/EL2004.template", "DEVICE=ECTest:4,PORT=OUT0,SCAN=I/O Intr")
+#dbLoadRecords("../../db/gadc.template", "DEVICE=ECTest:3,PORT=RF0,SCAN=.1 second,CHANNEL=1,SAMPLES=100840,INTSCAN=.1 second")
+#dbLoadRecords("../../db/xfc.template", "DEVICE=ECTest:3,PORT=RF0,SCAN=.1 second,CHANNEL=1")
 
 iocInit()
