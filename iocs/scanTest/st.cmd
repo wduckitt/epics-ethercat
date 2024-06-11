28feaa8fcca31e3ca89965cdb8303ca0e9b25b3f
William Duckitt <wdduckitt@sun.ac.za>
2024-06-11 09:17:29 +0000

    Refactor IOCs to use start_ioc.sh script for launching

--- iocs/scanTest/st.cmd
+++ iocs/scanTest/st.cmd
@@ -5,11 +5,12 @@ scantest_registerRecordDeviceDriver(pdbbase)
 #ADC_Ethercat_Sampler("RF0", 1, "Ch1Sample.Ch1Value", "Ch1CycleCount.Ch1CycleCount")
 
 ecAsynInit("/tmp/socket0", 1000000)
-
+dbLoadRecords
 dbLoadRecords("../../db/MASTER.template", "DEVICE=ECTest:0,PORT=MASTER0,SCAN=I/O Intr")
 dbLoadRecords("../../ethercatApp/Db/EK1818.template", "DEVICE=ECTest:1,PORT=ERIO.0,SCAN=I/O Intr")
-dbLoadRecords("../../ethercatApp/Db/EL6090.template", "DEVICE=ECTest:2,PORT=ERIO.1,SCAN=I/O Intr")
-dbLoadRecords("../../ethercatApp/Db/SAMD51.template", "DEVICE=ECTest:3,PORT=ERIO.2,SCAN=I/O Intr")
+dbLoadRecords("db/test.db", "DEVICE=ECTest:1")
+#dbLoadRecords("../../ethercatApp/Db/EL6090.template", "DEVICE=ECTest:2,PORT=ERIO.1,SCAN=I/O Intr")
+#dbLoadRecords("../../ethercatApp/Db/SAMD51.template", "DEVICE=ECTest:3,PORT=ERIO.2,SCAN=I/O Intr")
 #dbLoadRecords("../../db/EL1004.template", "DEVICE=ECTest:2,PORT=VACUUM0,SCAN=I/O Intr")
 #dbLoadRecords("../../db/EL3702.template", "DEVICE=ECTest:3,PORT=RF0,SCAN=I/O Intr")
 #dbLoadRecords("../../db/EL2004.template", "DEVICE=ECTest:4,PORT=OUT0,SCAN=I/O Intr")
