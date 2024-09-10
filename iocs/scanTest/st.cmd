b0cea6432b803efccdb498a9c0e3c3f186d57d3e
William Duckitt <wdduckitt@sun.ac.za>
2024-09-10 09:27:38 +0200

    clean up

--- iocs/scanTest/st.cmd
+++ iocs/scanTest/st.cmd
@@ -16,7 +16,7 @@ dbLoadRecords("../../ethercatApp/Db/EL2872.template", "DEVICE=HZBBDM:5,PORT=ERIO
 dbLoadRecords("db/HZB_DC.db", "device=HZB_RF_BUNCHER1_RFM_Trimmer,Pot=HZBBDM:3:INPUT1,MotorDrive=HZBBDM:4,CHANNEL=CHANNEL1,CWLimit=HZBBDM:4:DCMSTATUSCHANNEL1:STATUS__DIGITALINPUT1,CCWLimit=HZBBDM:4:DCMSTATUSCHANNEL1:STATUS__DIGITALINPUT2,Master=HZBBDM:0,EL3255=HZBBDM:3")
 
 
-##### Load Save files  #####################################################
+##### Load Save files  ######################
 set_savefile_path("save/")
 set_pass0_restoreFile("info_positions.sav")
 set_pass1_restoreFile("info_positions.sav")
@@ -26,8 +26,10 @@ iocInit()
 makeAutosaveFiles()
 create_monitor_set("info_positions.req", 1) 
 
-##### Sequencer ###########################################################################################
+##### Sequencer ##############################
 
 seq snlDCSequencer, "device=HZB_RF_BUNCHER1_RFM_Trimmer"
+
+dbcar * 1
 
 
